d9b6cb02199734459ea3565a01a1f23e2db21793
Gor Sayamyan <gor_s@trebel.io>
2026-03-28 13:07:47 +0400

    feat: added app initial configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -35,16 +34,41 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
-        compose = true
+        viewBinding = true
     }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.appcompat)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+
+    // UI
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
+    // RecyclerView (for list screen)
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+
+    // Swipe Refresh (pull-to-refresh requirement)
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0")
+
+    // Networking (Retrofit)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+    // Image loading (choose ONE, Coil recommended)
+    implementation("io.coil-kt:coil:2.7.0")
+
+    // Logging (optional but useful)
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
+    implementation(libs.androidx.navigation.fragment)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
